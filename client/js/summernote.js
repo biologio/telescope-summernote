a75f5fed2635eec2dae274a44dab5b8179e6caf0
Rashid Ahmad <decent10cs@gmail.com>
2015-10-30 14:18:13 +0800

    initial commit

---
+++ client/js/summernote.js
@@ -0,0 +1,43 @@
+Template.post_submit.rendered = function() {
+    Meteor.setTimeout(function() {
+        var $summernote = $("textarea[name='body']");
+        $summernote.summernote({
+            height: 400,
+            maxHeight: 1800,
+            minHeight: 250
+
+        });
+    }, 500)
+
+}
+Template.post_edit.rendered = function() {
+
+    Meteor.setTimeout(function() {
+        var $summernote = $("textarea[name='body']");
+        $summernote.summernote({
+            height: 400,
+            maxHeight: 1800,
+            minHeight: 250
+
+        });
+    }, 500)
+
+
+};
+
+Template.registerHelper("HTMLPurify", function(html) {
+    if (html) {
+        return UniHTML.purify(html)
+    }
+    return null;
+
+})
+
+Telescope.callbacks.add("postSubmitClient", function(postObject) {
+    postObject.body = UniHTML.purify(postObject.body);
+    return postObject;
+})
+Telescope.callbacks.add("postEditClient", function(postObject) {
+    postObject.$set.body = UniHTML.purify(postObject.$set.body);
+    return postObject;
+})
---
+++ package.js
@@ -0,0 +1,38 @@
+Package.describe({
+    name: 'biolog:telescope-summernote',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: '',
+    // URL to the Git repository containing the source code for this package.
+    git: '',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+    api.use('ecmascript');
+    api.use(["telescope:core0.25.0", "summernote:summernote@0.7.0",  "vazco:universe-html-purifier@1.2.3"]);
+
+
+    api.addFiles([
+        'client/templates/custom_post_body.html',
+        'client/js/summernote.js',
+        'client/scss/summernote.scss'
+
+    ], ['client']);
+    api.addFiles([
+        'lib/post.js'
+
+    ], ['server', 'client']);
+    api.addFiles('telescope-summernote.js');
+
+});
+
+Package.onTest(function(api) {
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('biolog:telescope-summernote');
+    api.addFiles('telescope-summernote-tests.js');
+});

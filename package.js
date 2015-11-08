5e9732f6835410070a552ab8686700890ff9c3a6
Rashid Ahmad <decent10cs@gmail.com>
2015-11-08 14:08:52 +0800

    minor changes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'biolog:telescope-summernote',
-    version: '0.1.2-3',
+    version: '0.1.1-4',
     // Brief, one-line summary of the package.
     summary: 'Summernote WYSIWYG editor with embedded images support for Telescope app',
     // URL to the Git repository containing the source code for this package.
